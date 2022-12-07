82c8b75a3b154d5642b986787e7a6137749c7ccc
Ram <Ramakrishnan.2.sankar@bt.com>
2022-12-07 18:42:33 +0000

    feat: Added Test cases  + modified readme

--- src/test/java/com/gt/service/CustomerManagementImplTest.java
+++ src/test/java/com/gt/service/CustomerManagementImplTest.java
@@ -16,13 +16,19 @@ public class CustomerManagementImplTest {
     List<Customer> customers = parser.parse("AddressBook");
 
     @Test
-    public void genderCounterTest(){
-        Assert.assertEquals(3,manager.genderCounter("female",  customers));
-        Assert.assertEquals(2,manager.genderCounter("male",  customers));
+    public void genderCounterTest() {
+        Assert.assertEquals(3, manager.genderCounter("female", customers));
+        Assert.assertEquals(2, manager.genderCounter("male", customers));
     }
 
     @Test
-    public void findOldestCustomerTest(){
-        Assert.assertEquals("Wes Jackson",manager.findOldestCustomer( customers));
+    public void findOldestCustomerTest() {
+        Assert.assertEquals("Wes Jackson", manager.findOldestCustomer(customers));
+    }
+
+    @Test
+    public void findAgeDifferenceCustomerTest() {
+        Assert.assertEquals("Bill McKnight is older than Paul Robinson by 7 Years, 9 Months, 30 Days",
+                manager.findAgeDifference(customers.get(0), customers.get(1)));
     }
 }
