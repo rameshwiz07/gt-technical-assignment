1c63d864a0c08accdd17ce71bd3d199bb0dadc19
Ram <Ramakrishnan.2.sankar@bt.com>
2022-12-07 17:54:37 +0000

    feat: Initial Code - added basic structure and log config

---
+++ src/main/java/com/gt/GTTechnicalAssignment.java
@@ -0,0 +1,15 @@
+package com.gt;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class GTTechnicalAssignment {
+    public static void main(String[] args)  {
+
+
+        log.info("##################################################################");
+
+
+    }
+}
